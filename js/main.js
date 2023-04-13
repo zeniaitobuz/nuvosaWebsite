52c6f67fa01b981c1962820e7eb8ee39cfc975a5
zenia banerjee <zenia@itobuz.com>
2023-04-13 14:37:00 -0700

    carousel changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 var swiper = new Swiper(".my-swiper", {
-  spaceBetween: 30,
-  centeredSlides: true,
+  centeredSlides: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   autoplay: {
-    delay: 2500,
+    delay: 1000,
     disableOnInteraction: false,
   },
   pagination: {
@@ -12,5 +15,19 @@ var swiper = new Swiper(".my-swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+    },
+    "@1.00": {
+      slidesPerView: 2.5,
+    },
+    "@1.50": {
+      slidesPerView: 3,
+    },
   },
 });
